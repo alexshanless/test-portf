d7e252539e1db43bee6dc4b04c7c74d2576980db
Alex Bilba <54191007+alexshanless@users.noreply.github.com>
2025-09-01 08:19:46 -0700

    featured projects

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -11,12 +11,18 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({
   request,
-}: Route.LoaderArgs): Promise<{ projects: Project[] }> {}
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+  const data = await res.json();
 
-const HomePage = () => {
+  return { projects: data };
+}
+
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
   return (
     <>
-      <FeaturedProjects />
+      <FeaturedProjects projects={projects} count={2} />
     </>
   );
 };
