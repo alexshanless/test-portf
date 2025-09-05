5fc67e960bb204abac44cb5b33ea7279b9689722
Alex Bilba <54191007+alexshanless@users.noreply.github.com>
2025-09-05 10:11:06 -0700

    blogs

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,10 +1,31 @@
-const BlogPage = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold text-white mb-2">Our Blog</h1>
-            <p className="text-lg text-gray-300 text-center">Welcome to the Blog page of The friendly dev!</p>
-        </div>
-    );
+import { Link } from 'react-router';
+import type { PostMeta } from '~/types';
+import type { Route } from './+types/index';
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL('/posts-meta.json', request.url);
+
+  const res = await fetch(url.href);
+  if (!res.ok) throw new Error('Failed to fetch data');
+
+  const data = await res.json();
+
+  return { posts: data };
 }
- 
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+  console.log(posts);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-white mb-2">Our Blog</h1>
+      <p className="text-lg text-gray-300 text-center">
+        Welcome to the Blog page of The friendly dev!
+      </p>
+    </div>
+  );
+};
+
 export default BlogPage;
