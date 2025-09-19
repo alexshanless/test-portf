2b24069b5201e25cfbe3fde9c031e4c2b60489fa
Alex Bilba <54191007+alexshanless@users.noreply.github.com>
2025-09-19 12:06:52 -0700

    update

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,45 +1,60 @@
-import ProjectCard from '~/components/ProjectCard';
-import type { Route } from './+types/index';
-import type { Project } from '~/types';
 import { useState } from 'react';
+import type { Route } from './+types/index';
+import type { Project, StrapiProject, StrapiResponse } from '~/types';
+import ProjectCard from '~/components/ProjectCard';
 import Pagination from '~/components/Pagination';
 import { AnimatePresence, motion } from 'framer-motion';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: 'The friendly dev | Projects' },
-    { name: 'description', content: 'Welcome to The friendly dev protfolio!' },
+    { title: 'The Friendly Dev | Projects' },
+    { name: 'description', content: 'My website project portfolio' },
   ];
 }
 
 export async function loader({
   request,
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/projects?populate=*`
+  );
+  const json: StrapiResponse<StrapiProject> = await res.json();
 
-  return { projects: data };
+  const projects = json.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url ? `${item.image.url}` : '/images/no-image.png',
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured,
+  }));
+
+  return { projects };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData as { projects: Project[] };
   const [selectedCategory, setSelectedCategory] = useState('All');
   const [currentPage, setCurrentPage] = useState(1);
-  const projectsPerPage = 2;
+  const projectsPerPage = 10;
 
-  //Get unqiue categories
+  const { projects } = loaderData as { projects: Project[] };
+
+  // Get unique categories
   const categories = [
     'All',
     ...new Set(projects.map((project) => project.category)),
   ];
 
-  //Filter projects based on the category
+  // Filter project based on the category
   const filteredProjects =
     selectedCategory === 'All'
       ? projects
       : projects.filter((project) => project.category === selectedCategory);
 
-  //Calculate total pages
+  // Calculate total pages
   const totalPages = Math.ceil(filteredProjects.length / projectsPerPage);
 
   // Get current pages projects
@@ -48,8 +63,9 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const currentProjects = filteredProjects.slice(indexOfFirst, indexOfLast);
 
   return (
-    <div>
-      <h2 className="text-3xl font-bold text-white mb-2">Our Projects</h2>
+    <>
+      <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
+
       <div className="flex flex-wrap gap-2 mb-8">
         {categories.map((category) => (
           <button
@@ -58,12 +74,17 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
               setSelectedCategory(category);
               setCurrentPage(1);
             }}
-            className={`px-3 py-1 rounded text-sm cursor-pointer ${selectedCategory === category ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-200'}`}
+            className={`px-3 py-1 rounded text-sm cursor-pointer ${
+              selectedCategory === category
+                ? 'bg-blue-600 text-white'
+                : 'bg-gray-700 text-gray-200'
+            }`}
           >
             {category}
           </button>
         ))}
       </div>
+
       <AnimatePresence mode="wait">
         <motion.div layout className="grid gap-6 sm:grid-cols-2">
           {currentProjects.map((project) => (
@@ -73,12 +94,13 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
           ))}
         </motion.div>
       </AnimatePresence>
+
       <Pagination
         totalPages={totalPages}
         currentPage={currentPage}
         onPageChange={setCurrentPage}
       />
-    </div>
+    </>
   );
 };
 
